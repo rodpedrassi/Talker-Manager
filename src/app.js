d2c2e64bc19f1d9569736e42cb1361398cdd121b
rodpedrassi <rodpedrassi@gmail.com>
2022-11-04 16:47:57 -0300

    requisito 4

--- src/app.js
+++ src/app.js
@@ -25,7 +25,28 @@ const {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   });
 
-  app.post('/login', async (req, res) => {
+  const validateLoginData = (req, res, next) => {
+    const loginData = { ...req.body };
+    if (!Object.keys(loginData).some((e) => e === 'email')) {
+      return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    } 
+    const validRegex = /[a-z0-9]+@[a-z]+\.[a-z]/;
+    if (!loginData.email.match(validRegex)) {
+      return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    }
+
+    if (!Object.keys(loginData).some((e) => e === 'password')) {
+      return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    } 
+    console.log(loginData.password.length);
+    if (loginData.password.length < 6) {
+      return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    } 
+    
+    next();
+  };
+
+  app.post('/login', validateLoginData, async (req, res) => {
     const token = crypto.randomBytes(8).toString('hex');
     return res.status(200).json({ token });
   });
